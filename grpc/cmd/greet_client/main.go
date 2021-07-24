5831f139eb9ad8f9ae07b7a66ceb26833805adc8
Allan Chepkoy <allankiplangat22@gmail.com>
2021-07-24 21:45:18 +0900

    Added client and server boilerplate

--- grpc/cmd/greet_client/main.go
+++ grpc/cmd/greet_client/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/chepkoyallan/protos/protos"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	fmt.Println("Hello from greet client")
+	// create connection to the server
+	cc, err := grpc.Dial("localhost:500051", grpc.WithInsecure())
+	if err != nil{
+		log.Fatalf("coluld not connect %v", err)
+	}
+
+	defer cc.Close()
+
+	c := protos.NewGreetServiceClient(cc)
+	fmt.Printf("Created Client: %f", c)
 }
