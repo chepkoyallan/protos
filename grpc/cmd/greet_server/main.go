e306b4fa8ed733d4aa26f917f20e92625e6ed3dc
Chepkoy Allan <allankiplangat22@gmail.com>
2021-07-24 18:23:29 +0900

    Added hello world server

--- grpc/cmd/greet_server/main.go
+++ grpc/cmd/greet_server/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/chepkoyallan/protos/github.com/chepkoyallan/protos"
+	"google.golang.org/grpc"
+)
+
+type server struct{
+	protos.UnimplementedGreetServiceServer
+}
 
 func main() {
 	fmt.Println("Hello from greet server")
+
+	// 1. Write a listener
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil{
+		log.Fatalf("Failed to listen:  %v", err)
+	}
+
+	// 2. We need to register a servive
+	s :=  grpc.NewServer()
+	protos.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to server: %v", err)
+	}
 }
