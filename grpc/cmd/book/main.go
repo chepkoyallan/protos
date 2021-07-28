c34501841d9782bb0250ff1711b0f5cb4d43004e
Chepkoy Allan <allankiplangat22@gmail.com>
2021-07-28 11:38:06 +0900

    Applied waitgroups

--- grpc/cmd/book/main.go
+++ grpc/cmd/book/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
+
 	"github.com/chepkoyallan/protos/pkg/book"
 )
 
@@ -11,21 +13,32 @@ var cache = map[int]book.Book{}
 var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main(){
+	//Wait groups
+	wg := &sync.WaitGroup{}
+
 	for i:=0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
-		if b, ok := queryCache(id); ok {
-			fmt.Println("From Cache")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("From Database")
-			fmt.Println(b)
-			continue
-		}
-		fmt.Printf("Book not found with id: '%v\n'", id)
-		time.Sleep(150 * time.Millisecond)
+		wg.Add(2)
+		go func(id int, wg *sync.WaitGroup){
+			if b, ok := queryCache(id); ok {
+				fmt.Println("From Cache")
+				fmt.Println(b)
+				
+			}
+			wg.Done()
+		}(id, wg)
+		go func(id int, wg *sync.WaitGroup){
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("From Database")
+				fmt.Println(b)
+			}
+			wg.Done()
+		}(id, wg)
+		
+		// fmt.Printf("Book not found with id: '%v\n'", id)
+		// time.Sleep(150 * time.Millisecond)
 	}
+	wg.Wait()
 	
 }
 
@@ -38,7 +51,7 @@ func queryDatabase(id int)(book.Book, bool){
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range book.Books {
 		if b.ID == id {
-			cache[id] = b
+			// cache[id] = b
 			return b, true
 		}
 	}
