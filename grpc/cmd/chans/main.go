5db735eb6279fd466982a8d5cb162ea0119222a1
Chepkoy Allan <allankiplangat22@gmail.com>
2021-07-28 14:37:49 +0900

    Using Channels with loops

--- grpc/cmd/chans/main.go
+++ grpc/cmd/chans/main.go
@@ -7,21 +7,27 @@ import (
 
 func main() {
 	wg := &sync.WaitGroup{}
-	ch := make(chan int, 1)
+	ch := make(chan int)
 
 	wg.Add(2)
 
 	// reciever go routine/channel
 	go func(ch <-chan int, wg *sync.WaitGroup){
-		// if message was sent or not
-		if msg, ok := <-ch; ok {
-			fmt.Println(msg, ok)
+		// if message was sent or
+		// if msg, ok := <-ch; ok {
+		// 	fmt.Println(msg, ok)
+		// }
+		for msg := range ch {
+			fmt.Println(msg)
 		}
 		wg.Done()
 	}(ch, wg)
 	
 	// Sender go routine/ send only
 	go func(ch chan<- int, wg *sync.WaitGroup){
+		for i :=0; i < 10; i++ {
+			ch <- i
+		}
 		close(ch)
 		wg.Done()
 	}(ch, wg)
